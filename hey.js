2d8fd4d37b8cc6e079c0a595150823317bfb8a1f
sathwik123677 <sathwikp06@gmail.com>
2025-05-22 12:09:57 +0530

    addded read more

--- hey.js
+++ hey.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
     res.render("index", { files: files });
   });
 });
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show",{filename: req.params.filename, filedata: filedata});
+  });
+});
 app.post("/create", (req, res) => {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
