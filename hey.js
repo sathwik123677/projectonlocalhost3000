595d1b23d21b86cb9a9eb917b2bfc23d323f43fa
sathwik123677 <sathwikp06@gmail.com>
2025-05-22 11:24:45 +0530

    completed upto creeating tasks

---
+++ hey.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const Path = require("path");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join("__dirname", "Public")));
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+app.listen(port);
